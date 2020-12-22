8513e1d093369171740e73faf99871cc794aef1a
unknown <saikiranrudra2@gmail.com>
2020-12-22 11:18:39 +0530

    fixed issue of with no reviews

--- app.py
+++ app.py
@@ -28,76 +28,82 @@ def reviews():
     # check the availability in DB
     product_from_db = products.find_one({"product_name": search_string})
 
-    if product_from_db is None:
-        url = search_url + search_string.replace(" ", '+')  # url for searcing products
-        products_page = fetcher(url)  # fetching html page of products
-        products_soup = BeautifulSoup(products_page, "html.parser")
+    try:
+        if product_from_db is None:
+            url = search_url + search_string.replace(" ", '+')  # url for searcing products
+            products_page = fetcher(url)  # fetching html page of products
+            products_soup = BeautifulSoup(products_page, "html.parser")
 
-        product_anchor_tag = products_soup.find("a", {"class": "_1fQZEK"})
-        if product_anchor_tag is None:
-            product_anchor_tag = products_soup.find("a", {"class": "_2rpwqI"})
+            product_anchor_tag = products_soup.find("a", {"class": "_1fQZEK"})
+            if product_anchor_tag is None:
+                product_anchor_tag = products_soup.find("a", {"class": "_2rpwqI"})
 
-        if product_anchor_tag is not None:
-            product_route = product_anchor_tag.get("href")  # extracting product url
-            product_link = f"{base_url}{product_route}"  # product link
-            product_page = fetcher(product_link)
+            if product_anchor_tag is not None:
+                product_route = product_anchor_tag.get("href")  # extracting product url
+                product_link = f"{base_url}{product_route}"  # product link
+                product_page = fetcher(product_link)
 
-            product_soup = BeautifulSoup(product_page, "html.parser")
-            reviews_route = product_soup.find("div", "_3UAT2v _16PBlm").parent.get("href")
+                product_soup = BeautifulSoup(product_page, "html.parser")
+                reviews_route = product_soup.find("div", "_3UAT2v _16PBlm")
+                if reviews_route is not None:
+                    reviews_route = reviews_route.parent.get("href")
 
-            # removing all string after &aid including &aid
-            filter_index = reviews_route.find("&aid")
-            reviews_route = reviews_route[0:filter_index]
-            reviews_link = f"{base_url}{reviews_route}"
-            reviews_page = fetcher(reviews_link)
-            reviews_soup = BeautifulSoup(reviews_page, "html.parser")
+                # removing all string after &aid including &aid
+                if reviews_route is None:
+                    raise Exception("Unable to scrape Reviews")
 
-            # Extracing comments for html
-            reviews_divs = reviews_soup.find_all("div", class_="col _2wzgFH K0kLPL")
-            reviews = []
+                filter_index = reviews_route.find("&aid")
+                reviews_route = reviews_route[0:filter_index]
+                reviews_link = f"{base_url}{reviews_route}"
+                reviews_page = fetcher(reviews_link)
+                reviews_soup = BeautifulSoup(reviews_page, "html.parser")
 
-            for review in reviews_divs:
-                rating_tag = review.find("div", class_="_3LWZlK")
-                comment_tag = review.find("div", class_="")
+                # Extracing comments for html
+                reviews_divs = reviews_soup.find_all("div", class_="col _2wzgFH K0kLPL")
+                reviews = []
 
-                heading = review.find("p", class_="_2-N8zT").string
-                rating = re.sub(r"<div\sclass=\"(\w\s*)+\">", "", str(rating_tag))
-                comment = str(comment_tag.findChildren("div", recursive=False)[0]) \
-                    .replace("<div class=\"\">", "") \
-                    .replace("</div>", "") \
-                    .replace("<br/>", "")
-                reviews.append({"heading": heading, "rating": rating[0], "comment": comment})
+                for review in reviews_divs:
+                    rating_tag = review.find("div", class_="_3LWZlK")
+                    comment_tag = review.find("div", class_="")
 
-            sentiment = calculate_sentiment(reviews)
+                    heading = review.find("p", class_="_2-N8zT").string
+                    rating = re.sub(r"<div\sclass=\"(\w\s*)+\">", "", str(rating_tag))
+                    comment = str(comment_tag.findChildren("div", recursive=False)[0]) \
+                        .replace("<div class=\"\">", "") \
+                        .replace("</div>", "") \
+                        .replace("<br/>", "")
+                    reviews.append({"heading": heading, "rating": rating[0], "comment": comment})
 
-            total = len(reviews)
-            total = total if total > 0 else 1
+                sentiment = calculate_sentiment(reviews)
 
-            sentiment[0] = ((sentiment[0] / total) * 100)
-            sentiment[1] = ((sentiment[1] / total) * 100)
-            sentiment[2] = ((sentiment[2] / total) * 100)
+                total = len(reviews)
+                total = total if total > 0 else 1
 
-            if len(reviews) != 0:
-                # insert it into db
-                products.insert_one({"product_name": search_string, "reviews": reviews})
+                sentiment[0] = ((sentiment[0] / total) * 100)
+                sentiment[1] = ((sentiment[1] / total) * 100)
+                sentiment[2] = ((sentiment[2] / total) * 100)
 
-                return render_template(
-                    "reviews.html",
-                    len=len(reviews),
-                    title=search_string,
-                    reviews=reviews,
-                    sentiment=sentiment
-                )
+                if len(reviews) != 0:
+                    # insert it into db
+                    products.insert_one({"product_name": search_string, "reviews": reviews})
 
-            else:
-                # print no reviews to print
-                return render_template(
-                    "review.html",
-                    len=1,
-                    title=search_string,
-                    reviews=["No Reviews available for this product"],
-                    sentiment=sentiment
-                )
+                    return render_template(
+                        "reviews.html",
+                        len=len(reviews),
+                        title=search_string,
+                        reviews=reviews,
+                        sentiment=sentiment
+                    )
+
+                else:
+                    # print no reviews to print
+                    return render_template(
+                        "review.html",
+                        len=1,
+                        title=search_string,
+                        reviews=["No Reviews available for this product"],
+                        sentiment=sentiment
+                    )
         else:
             # render fetched from db
             # calculating sentiment
@@ -116,16 +122,15 @@ def reviews():
                 reviews=product_from_db["reviews"],
                 sentiment=sentiment
             )
-    else:
-        # print no reviews to print
+
+    except Exception as e:
         return render_template(
-            "review.html",
+            "reviews.html",
             len=1,
             title=search_string,
-            reviews=["No Reviews available for this product"],
+            reviews=[{"heading": "No Reviews", "comment": "no reviews available for this product to scrap", "comment": 5}],
             sentiment=[0, 0, 0]
         )
-
 
 if __name__ == "__main__":
     app.run()
